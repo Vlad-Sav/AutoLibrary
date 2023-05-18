9ea9a330ebe17f5ef1049ad246e38fd345ca5bae
Vlad-Sav <savchenko_vladlen2511@mail.ru>
2023-05-18 13:03:30 +0600

    Added car saving to database

--- app/src/main/java/kg/android/autolibrary/ui/cars/CarsFragment.kt
+++ app/src/main/java/kg/android/autolibrary/ui/cars/CarsFragment.kt
@@ -1,16 +1,20 @@
 package kg.android.autolibrary.ui.cars
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import kg.android.autolibrary.R
 import kg.android.autolibrary.databinding.FragmentCarsBinding
+import kg.android.autolibrary.ui.addcar.AddCarUiEvent
 
 @AndroidEntryPoint
 class CarsFragment : Fragment(), IOnCarItemClicked {
@@ -29,6 +33,7 @@ class CarsFragment : Fragment(), IOnCarItemClicked {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.readAllCars()
         viewModel.cars.observe(viewLifecycleOwner) { cars ->
             if(cars != null && cars.isNotEmpty()){
                 binding.carsRecyclerView.apply {
@@ -38,11 +43,18 @@ class CarsFragment : Fragment(), IOnCarItemClicked {
                 }
             }
         }
-        viewModel.addCar()
-        viewModel.readAllCars()
+        binding.addBtn.setOnClickListener{ addCarOnClick() }
+    }
+
+    private fun addCarOnClick(){
+        val directions =
+            CarsFragmentDirections.actionCarsFragmentToAddCarFragment()
+        findNavController().navigate(directions)
     }
 
     override fun onCarItemClicked(id: Int) {
 
     }
+
+
 }
--- app/src/main/java/kg/android/autolibrary/ui/cars/CarsViewModel.kt
+++ app/src/main/java/kg/android/autolibrary/ui/cars/CarsViewModel.kt
@@ -7,27 +7,72 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kg.android.autolibrary.data.models.Car
 import kg.android.autolibrary.data.repository.CarsRepository
+import kg.android.autolibrary.ui.addcar.AddCarResult
+import kg.android.autolibrary.ui.addcar.AddCarState
+import kg.android.autolibrary.ui.addcar.AddCarUiEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class CarsViewModel @Inject constructor(
     private val repository: CarsRepository
 ): ViewModel() {
+    var state = MutableLiveData<AddCarState>()
     lateinit var cars: LiveData<List<Car>>
 
+    private val resultChannel = Channel<AddCarResult<Unit>>()
+    val addCarResult = resultChannel.receiveAsFlow()
+
     init {
-        readAllCars()
+        state.postValue(AddCarState())
+    }
+
+    fun onAddCarEvent(event: AddCarUiEvent) {
+        when(event) {
+            is AddCarUiEvent.CarNameChanged -> {
+                state.value = state.value?.copy(carName = event.value)
+            }
+            is AddCarUiEvent.ReleaseYearChanged -> {
+                state.value = state.value?.copy(releaseYear = event.value)
+            }
+            is AddCarUiEvent.EngineCapacityChanged -> {
+                state.value = state.value?.copy(engineCap = event.value)
+            }
+            is AddCarUiEvent.AddCar -> {
+                addCar()
+            }
+        }
     }
 
     fun readAllCars() {
-            cars = repository.readAllCars
-
+        cars = repository.readAllCars
     }
 
     fun addCar() {
         viewModelScope.launch {
-            val result = repository.addCar(Car(7,"car","",2005,10.0,""))
+            val currentTime = LocalDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            val formatted = currentTime.format(formatter)
+            try {
+                repository.addCar(
+                    Car(
+                        0,
+                        state.value?.carName ?: "",
+                        "",
+                        state.value?.releaseYear?.toInt() ?: 0,
+                        state.value?.engineCap?.toDouble() ?: 0.0,
+                        formatted)
+                )
+                resultChannel.send(AddCarResult.Added())
+            }
+            catch (e: Exception){
+                resultChannel.send(AddCarResult.Error(e.message ?: "Unknown error"))
+            }
         }
     }
 }
