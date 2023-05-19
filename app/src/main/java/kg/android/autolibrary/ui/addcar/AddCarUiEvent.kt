cacba8a9c8c564dbdca0edef1e278fe72abaa05b
Vlad-Sav <savchenko_vladlen2511@mail.ru>
2023-05-19 23:29:30 +0600

    Added photo picker

--- app/src/main/java/kg/android/autolibrary/ui/addcar/AddCarUiEvent.kt
+++ app/src/main/java/kg/android/autolibrary/ui/addcar/AddCarUiEvent.kt
@@ -7,6 +7,7 @@ sealed class AddCarUiEvent {
     data class CarNameChanged(val value: String): AddCarUiEvent()
     data class ReleaseYearChanged(val value: String): AddCarUiEvent()
     data class EngineCapacityChanged(val value: String): AddCarUiEvent()
+    data class PhotoChanged(val value: String): AddCarUiEvent()
     object AddCar: AddCarUiEvent()
     object UpdateUserPermissions: AddCarUiEvent()
 }
